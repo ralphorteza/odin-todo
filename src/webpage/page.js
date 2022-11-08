a5ce9ee965baa96868c41cc0fb055205e15b11a3
ralph orteza <ralphlorteza@gmail.com>
2022-11-08 14:35:19 -0800

    Initialize factory functions in page.js

---
+++ src/webpage/page.js
@@ -0,0 +1,31 @@
+
+// TODO: header
+const makeHeader = () => {
+
+}
+
+// TODO: footer
+const makeFooter = () => {
+
+}
+
+// TODO: main
+const makeMain = () => {
+
+}
+
+// TODO: card
+const makeCard = () => {
+
+}
+
+// TODO: sidebar
+const makeSidebar = () => {
+
+}
+
+// TODO: webpage
+const makeWebpage = () => {
+
+}
+
