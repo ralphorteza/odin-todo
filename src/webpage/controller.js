3f66686815b1c6a2cb3bae9b03f639b5db7e7253
ralph orteza <ralphlorteza@gmail.com>
2022-11-16 11:06:44 -0800

    Create _createTaskCard function in controller.js

--- src/webpage/controller.js
+++ src/webpage/controller.js
@@ -1,25 +1,40 @@
 
-const createFormController = () => {
-  const openFormBtns = document.querySelector('#add-task');
-  const closeFormBtns = document.querySelector('#cancel-task');
+import { makeCard } from "./page.js";
 
+const createFormController = () => {
+  const main = document.getElementById('main');
   const form = document.getElementById('form-container');
   const overlay = document.getElementById('overlay');
 
-  const  _openForm = (form, overlay) => {
-    if (form == null) return
-    form.classList.add('active')
-    overlay.classList.add('active')
+  const openFormBtns = document.querySelector('#add-task');
+  const closeFormBtns = document.querySelector('#cancel-task');
+  const createTaskBtn = document.querySelector('#create-task');
+
+  const _openForm = (form, overlay) => {
+    if (form == null) return;
+    form.classList.add('active');
+    overlay.classList.add('active');
   };
-  
+
   const _closeForm = (form, overlay) => {
-    if (form == null) return
-    form.classList.remove('active')
-    overlay.classList.remove('active')
+    if (form == null) return;
+    form.classList.remove('active');
+    overlay.classList.remove('active');
+  };
+
+  // TODO: push information into card
+  const _createTaskCard = () => {
+    const card = makeCard();
+    main.append(card);
+    _closeForm(form, overlay);
   };
 
   openFormBtns.addEventListener('click', () => {
     _openForm(form, overlay)
+  });
+
+  createTaskBtn.addEventListener('click', () => {
+    _createTaskCard();
   });
 
   overlay.addEventListener('click', () => {
