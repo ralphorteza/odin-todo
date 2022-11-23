b5bd24f094b04b6b102cc8546f5e1abc4f576de3
ralph orteza <ralphlorteza@gmail.com>
2022-11-22 19:42:30 -0800

    Create class task in task.js

---
+++ src/task.js
@@ -0,0 +1,29 @@
+export default class task {
+  constructor(title, description, priority, date, id) {
+    this.title = title;
+    this.description = description;
+    this.priority = priority;
+    this.date = date;
+    this.id = id;
+  }
+
+  setTitle(title) {
+    this.title = title;
+  }
+
+  setDescription(description) {
+    this.description = description;
+  }
+
+  setPriority(priority) {
+    this.priority = priority;
+  }
+
+  setDate(date) {
+    this.date = date;
+  }
+
+  getId() {
+    return this.id;
+  }
+}
