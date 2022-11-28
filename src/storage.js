a2f0c5b2c30c67bff03b3c96665264d39941a023
ralph orteza <ralphlorteza@gmail.com>
2022-11-28 12:47:17 -0800

    Initialize storage.js

---
+++ src/storage.js
@@ -0,0 +1,63 @@
+import Project from './project';
+import Task from './task';
+import ToDoList from './todolist';
+
+export default class Storage {
+  static saveToDoList(data) {
+    localStorage.setItem('toDoList', JSON.stringify(data));
+  }
+
+  static getToDoList() {
+    const toDoList = Object.assign(
+      new ToDoList(),
+      JSON.parse(localStorage.getItem('toDoList')),
+    );
+
+    toDoList.setProjects(
+      toDoList
+        .getProjects()
+        .map((project) => Object.assign(new Project(), project)),
+    );
+
+    toDoList.getProjects()
+      .forEach((project) => {
+        project.setTasks(
+          project.getTasks().map((task) => Object.assign(new Task(), task)),
+        );
+      });
+
+    return toDoList;
+  }
+
+  static addProject(project) {
+    const toDoList = Storage.getToDoList();
+    toDoList.addProject(project);
+    Storage.saveToDoList(toDoList);
+  }
+
+  static deleteProject(projectName) {
+    const toDoList = Storage.getToDoList();
+    toDoList.deleteProject(projectName);
+    Storage.saveToDoList(toDoList);
+  }
+
+  static addTask(projectName, task) {
+    const toDoList = Storage.getToDoList();
+    toDoList.getProject(projectName).addTask(task);
+    Storage.saveToDoList(toDoList);
+  }
+
+  static deleteTask(projectName, taskName) {
+    const toDoList = Storage.getToDoList();
+    toDoList.getProject(projectName).deleteTask(taskName);
+    Storage.saveToDoList(toDoList);
+  }
+
+  static renameTask(projectName, taskName, newTaskName) {
+
+  }
+
+  static setTaskDate(projectName, taskName, newDueDate) {
+
+  }
+}
