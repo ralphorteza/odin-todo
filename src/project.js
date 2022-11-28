91ff56129cf4af55c457ba89ee755cc9b4438de8
ralph orteza <ralphlorteza@gmail.com>
2022-11-28 12:50:19 -0800

    Add getTask() and contains() in project.js

--- src/project.js
+++ src/project.js
@@ -20,6 +20,14 @@ export default class Project {
     return this.tasks;
   }
 
+  getTask(taskName) {
+    return this.task.find((task) => task.getName() === taskName);
+  }
+
+  contains(taskName) {
+    return this.tasks.some((task) => task.getName() === taskName);
+  }
+
   addTask(newTask) {
     if (this.tasks.find((task) => task.getName() === newTask.name)) return;
     this.tasks.push(newTask);
