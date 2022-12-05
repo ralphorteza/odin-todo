51e6fa0c411e480cdefa22a4288617dbd434f1b5
ralph orteza <ralphlorteza@gmail.com>
2022-12-05 10:58:37 -0800

    Create task form event handlers

--- src/dom.js
+++ src/dom.js
@@ -9,16 +9,18 @@ export default class Dom {
   static loadPage() {
     Dom.taskCardTesting();
     Handler.forAddTaskButton();
+    Handler.forTaskFormButtons();
   }
 
   static taskCardTesting() {
-    const task1 = new Task('example 1', '12/15/2022');
+    const task1 = new Task('example 1', 123, '12/15/2022');
     Render.aTaskCard(task1.getName(), task1.getDate());
 
-    const task2 = new Task('example 2', '11/15/2022');
+    const task2 = new Task('example 2', 3435, '11/15/2022');
     Render.aTaskCard(task2.getName(), task2.getDate());
 
-    const task3 = new Task('example 3', '10/15/2022');
+    const task3 = new Task('example 3', 5324, '10/15/2022');
+    console.log(task3.getDate);
     Render.aTaskCard(task3.getName(), task3.getDate());
   }
 }
--- src/handler.js
+++ src/handler.js
@@ -1,3 +1,6 @@
+import Task from './task';
+import Render from './render';
+
 export default class Handler {
   // TODO: Figure out eventlistner functions for task buttons.
   static forTaskButtons() {
@@ -13,6 +16,11 @@ export default class Handler {
     });
   }
 
+  static formHandlers() {
+    Handler.forAddTaskButton();
+    Handler.forTaskFormButtons();
+  }
+
   static forAddTaskButton() {
     const addTaskButton = document.querySelector('#add-task');
     addTaskButton.addEventListener('click', Handler.openTaskForm);
@@ -26,6 +34,15 @@ export default class Handler {
     formContainer.classList.add('active');
   }
 
+  static forTaskFormButtons() {
+    // const createTaskButton = document.querySelector('#create-task');
+    const cancelTaskButton = document.querySelector('#cancel-task');
+    const form = document.querySelector('#form');
+
+    cancelTaskButton.addEventListener('click', Handler.cancelTaskForm);
+    form.addEventListener('submit', Handler.createTask);
+  }
+
   static cancelTaskForm() {
     const overlay = document.querySelector('#overlay');
     const formContainer = document.querySelector('#form-container');
@@ -34,7 +51,16 @@ export default class Handler {
     formContainer.classList.remove('active');
   }
 
-  static forTaskForm() {
+  static createTask(event) {
+    event.preventDefault();
+    // const projectName = document.querySelector('#project-name').textContent;
+    const taskName = document.querySelector('#task-title').value;
+    const date = document.querySelector('#task-date').value;
 
+    const newTask = new Task(taskName, date);
+    Render.aTaskCard(newTask.getName(), newTask.getDate());
+
+    document.querySelector('#form-container').classList.remove('active');
+    document.querySelector('#overlay').classList.remove('active');
   }
 }
