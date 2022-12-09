b9f29139ebea757ed448dd5000691c1da2cb5b03
ralph orteza <ralphlorteza@gmail.com>
2022-12-09 13:53:36 -0800

    Remove redundant add button in index.html

--- src/todolist.js
+++ src/todolist.js
@@ -1,35 +1,39 @@
-import Project from './project';
-// import Task from './task';
+// static projectsListTesting() {
+//   // const projectsList = ProjectsList.getProjectsList();
 
-export default class ToDoList {
-  constructor() {
-    this.projects = [];
-    this.projects.push(new Project('Inbox'));
-  }
+//   const projects = Dom.exampleProjects();
+//   const tasks = Dom.exampleTasks();
 
-  setProjects(projects) {
-    this.projects = projects;
-  }
+//   // Storage.deleteProject(projects.testProject2);
+//   // Storage.addProject(projects.testProject2);
+//   // Storage.addTask(projects.testProject2.name, tasks.task3);
+//   Storage.deleteTask(projects.testProject2.name, tasks.task2.name);
+// }
 
-  getProjects() {
-    return this.projects;
-  }
+// static exampleTasks() {
+//   const task1 = new Task('example 1', '12/15/2022');
+//   Render.aTaskCard(task1.getName(), task1.getDate());
 
-  getProject(projectName) {
-    return this.projects.find((project) => project.getName() === projectName);
-  }
+//   const task2 = new Task('example 2', '11/15/2022');
+//   Render.aTaskCard(task2.getName(), task2.getDate());
 
-  contains(projectName) {
-    return this.projects.some((project) => project.getName() === projectName);
-  }
+//   const task3 = new Task('example 3', '10/15/2022');
+//   Render.aTaskCard(task3.getName(), task3.getDate());
 
-  addProject(newProject) {
-    if (this.projects.find((project) => project.name === newProject.name)) return;
-    this.projects.push(newProject);
-  }
+//   const task4 = new Task('example 4', '9/15/2022');
+//   Render.aTaskCard(task4.getName(), task4.getDate());
 
-  deleteProject(projectName) {
-    const index = this.projects.indexOf((project) => project.getName() === projectName);
-    this.projects.splice(index, 1);
-  }
-}
+//   const task5 = new Task('example 5', '8/15,2022');
+//   Render.aTaskCard(task5.getName(), task5.getDate());
+
+//   return {
+//     task1, task2, task3, task4, task5,
+//   };
+// }
+
+// static exampleProjects() {
+//   const testProject1 = new Project('testProject1');
+//   const testProject2 = new Project('testProject2');
+
+//   return { testProject1, testProject2 };
+// }
