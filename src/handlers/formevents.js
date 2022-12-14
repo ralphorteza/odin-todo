c1d120ab18a4b0463cfba2441308847d5b11b6f5
ralph orteza <ralphlorteza@gmail.com>
2022-12-14 12:25:33 -0800

    Initialize initTaskBUttons() in formevents.js

--- src/handlers/formevents.js
+++ src/handlers/formevents.js
@@ -1,5 +1,7 @@
 import Task from '../task';
 import Render from '../render';
+import Storage from '../storage';
+import TaskEvents from './taskevents';
 
 export default class FormEvents {
   // FORM EVENT HANDLERS CODE BLOCKS BELOW. //
@@ -41,14 +43,38 @@ export default class FormEvents {
 
   static createTask(event) {
     event.preventDefault();
-    // const projectName = document.querySelector('#project-name').textContent;
+    const projectName = document.querySelector('#project-name-header').textContent;
     const taskName = document.querySelector('#task-title').value;
-    const date = document.querySelector('#task-date').value;
+    const taskDate = document.querySelector('#task-date').value;
 
-    const newTask = new Task(taskName, date);
-    Render.aTaskCard(newTask.getName(), newTask.getDate());
+    Storage.addTask(projectName, taskName);
+    // Storage.setTaskDate(projectName, taskName, taskDate);
 
-    document.querySelector('#form-container').classList.remove('active');
-    document.querySelector('#overlay').classList.remove('active');
+    Render.aTaskCard(taskName, taskDate);
+
+    console.log(`Task created in project ${projectName}!`);
   }
+
+  // TODO: initialize task buttons upon task creation.
+  static initTaskButtons() {
+    // const editTaskButtons = document.querySelector('.button-edit-task');
+    const deleteTaskButtons = document.querySelector('.button-delete-task');
+
+    deleteTaskButtons.forEach((deleteTaskButton) => {
+      deleteTaskButton.addEventListener('click', TaskEvents.deleteTask);
+    });
+  }
+
+  // static createTask(event) {
+  //   event.preventDefault();
+  //   // const projectName = document.querySelector('#project-name').textContent;
+  //   const taskName = document.querySelector('#task-title').value;
+  //   const date = document.querySelector('#task-date').value;
+
+  //   const newTask = new Task(taskName, date);
+  //   Render.aTaskCard(newTask.getName(), newTask.getDate());
+
+  //   document.querySelector('#form-container').classList.remove('active');
+  //   document.querySelector('#overlay').classList.remove('active');
+  // }
 }
