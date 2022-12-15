28fa8ab9ea96f97e53e7d46547ab88c81a4d1949
ralph orteza <ralphlorteza@gmail.com>
2022-12-14 16:07:21 -0800

    Add cancelTaskEdit() function in taskevents.js

--- src/handlers/taskevents.js
+++ src/handlers/taskevents.js
@@ -18,12 +18,12 @@ export default class TaskEvents {
 
   // TODO: Figure out eventlistner functions for task buttons.
   static initTaskButtons() {
-    // const taskEditButtons = document.querySelectorAll('.button-edit-task');
+    const taskEditButtons = document.querySelectorAll('.button-edit-task');
     const taskDeleteButtons = document.querySelectorAll('.button-delete-task');
 
-    // taskEditButtons.forEach((taskEditButton) => {
-    //   taskEditButton.addEventListener('click', TaskEvents.editTask);
-    // });
+    taskEditButtons.forEach((taskEditButton) => {
+      taskEditButton.addEventListener('click', TaskEvents.editTaskHandler);
+    });
 
     taskDeleteButtons.forEach((taskDeleteButton) => {
       taskDeleteButton.addEventListener('click', TaskEvents.deleteTask);
@@ -39,5 +39,43 @@ export default class TaskEvents {
     taskCard.remove();
 
     console.log(`delete task ${taskName} in project ${projectName}!`);
+  }
+
+  static editTaskHandler(event) {
+    const projectName = document.querySelector('#project-name-header').textContent;
+    const taskCard = event.target.parentElement.parentElement;
+    const taskName = taskCard.children[0].children[1].textContent;
+    const taskDate = taskCard.children[1].children[0].textContent;
+    const currentTaskName = document.querySelector('#current-task-name');
+    const taskNameInput = document.querySelector('#edit-task-title');
+    const taskDateInput = document.querySelector('#edit-task-date');
+    const overlay = document.querySelector('#overlay');
+    const editTaskForm = document.querySelector('#form-edit-task-container');
+
+    currentTaskName.textContent = taskName;
+    currentTaskName.style.display = 'none';
+
+    taskNameInput.placeholder = taskName;
+    taskDateInput.value = taskDate;
+
+    overlay.classList.add('active');
+    editTaskForm.classList.add('active');
+
+    // const editTaskButton = document.querySelector('#create-edit-task');
+    const cancelEditTaskButton = document.querySelector('#cancel-edit-task');
+    console.log(`Editing task ${taskName} in project ${projectName}!`);
+
+    cancelEditTaskButton.addEventListener('click', TaskEvents.cancelTaskEdit);
+  }
+
+  static cancelTaskEdit() {
+    const overlay = document.querySelector('#overlay');
+    const editTaskForm = document.querySelector('#form-edit-task-container');
+    const currentTaskName = document.querySelector('#current-task-name').textContent;
+    const projectName = document.querySelector('#project-name-header').textContent;
+
+    overlay.classList.remove('active');
+    editTaskForm.classList.remove('active');
+    console.log(`Cancelled editing task ${currentTaskName} in project ${projectName}!`);
   }
 }
